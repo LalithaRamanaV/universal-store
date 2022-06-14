2027250c7214e89c203d38862f260d41aef77259
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-14 18:59:09 +0530

    build: update categoryItem

--- universe/src/components/CategoryItem.jsx
+++ universe/src/components/CategoryItem.jsx
@@ -2,12 +2,48 @@ import React from 'react'
 import styled from "styled-components"
 
 const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`
+const Button = styled.button`
+border:none;
+padding: 10px;
+background-color: white;
+color:gray;
+cursor: pointer;
+font-weight: 600;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Info = styled.div`
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+const Title = styled.h1`
+    color:white;
+    margin-bottom: 20px;
 `
 
 const CategoryItem = ({item}) => {
   return (
     <Container>
-  hi
+    <Image src={item.img}/>
+    <Info>
+        <Title>{item.title}</Title>
+        <Button>Shop Now</Button>
+    </Info>
     </Container>
   )
 }
