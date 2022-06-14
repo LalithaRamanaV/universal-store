f42019fd8286e61cfe4a583ff1dbea8938d741e0
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-14 23:31:56 +0530

    feat: added product list

--- universe/src/App.jsx
+++ universe/src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <ProductList/>
     </div>
   );
 }
