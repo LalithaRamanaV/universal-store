05a07a9accf211aad4dfd332313f6ba3edff04e0
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-15 00:29:28 +0530

    feat: added register

--- universe/src/App.js
+++ universe/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Login from './pages/Login';
 
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register/>
     </div>
   );
 }
