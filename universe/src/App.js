badc04281c84b98bed8f840393bbaa266ca13d26
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-15 08:10:52 +0530

    docs: updated readme

--- universe/src/App.js
+++ universe/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Cart from './pages/Cart';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Cart/>
+      <Home/>
     </div>
   );
 }
